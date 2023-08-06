ae266f224156d64fbe1ca757cd2aa5580791ae3b
zachlewis23 <46308319+zachlewis23@users.noreply.github.com>
2023-08-06 11:22:23 -0500

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,19 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+  //console.log( "ready!" );
+
+  $("#imed1316-gallery").nanogallery2({
+    //nanogallery
+    thumbnailWidth: "300",
+    thumbnailHeight: "auto",
+    thumbnailBorderVertical: 0,
+    thumbnailBorderHorizontal: 0,
+    thumbnailLabel: {
+      position: "overImageOnBottom",
+      display: false
+    },
+    thumbnailHoverEffect2: "imageScaleIn80|imageSepiaOff|labelAppear75",
+    thumbnailAlignment: "center",
+    thumbnailOpenImage: true
+  }); //nanogallery
+}); //document ready
